5988a9ae3de4bb1cfaa841b7ab90ee661bed2fa0
Rana Mahad Ahmer <ranamahadahmer@outlook.com>
2024-12-22 16:30:05 +0500

    -

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import os
 from asyncio import run
 from dataclasses import dataclass
 
-from ipv8.ipv8.dht.community import Community, CommunitySettings
-from ipv8.ipv8.configuration import ConfigBuilder, Strategy, WalkerDefinition, default_bootstrap_defs
-from ipv8.ipv8.lazy_community import lazy_wrapper
-from ipv8.ipv8.messaging.payload_dataclass import DataClassPayload
-from ipv8.ipv8.types import Peer
-from ipv8.ipv8.util import run_forever
-
-from ipv8.ipv8_service import IPv8
+from ipv8.community import Community, CommunitySettings
+from ipv8.configuration import ConfigBuilder, Strategy, WalkerDefinition, default_bootstrap_defs
+from ipv8.lazy_community import lazy_wrapper
+from ipv8.messaging.payload_dataclass import DataClassPayload
+from ipv8.types import Peer
+from ipv8.util import run_forever
+from ipv8_service import IPv8
 
 
 @dataclass
@@ -53,15 +52,15 @@ class MyCommunity(Community):
 
 
 async def start_communities() -> None:
-    for i in [1, 2, 3]:
-        builder = ConfigBuilder().clear_keys().clear_overlays()
-        builder.add_key("my peer", "medium", f"ec{i}.pem")
-        builder.add_overlay("MyCommunity", "my peer",
-                            [WalkerDefinition(Strategy.RandomWalk,
-                                              10, {'timeout': 3.0})],
-                            default_bootstrap_defs, {}, [('started',)])
-        await IPv8(builder.finalize(),
-                   extra_communities={'MyCommunity': MyCommunity}).start()
+
+    builder = ConfigBuilder().clear_keys().clear_overlays()
+    builder.add_key("my peer", "medium", f"ec{1}.pem")
+    builder.add_overlay("MyCommunity", "my peer",
+                        [WalkerDefinition(Strategy.RandomWalk,
+                                          10, {'timeout': 3.0})],
+                        default_bootstrap_defs, {}, [('started',)])
+    await IPv8(builder.finalize(),
+               extra_communities={'MyCommunity': MyCommunity}).start()
     await run_forever()
 
 
